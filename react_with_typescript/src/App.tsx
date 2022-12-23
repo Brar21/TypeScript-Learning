02b2b24dc5302c7a3d1d18a750054fb673e70899
Brar21 <103635395+Brar21@users.noreply.github.com>
2022-12-23 09:58:14 +0530

    day-4

--- react_with_typescript/src/App.tsx
+++ react_with_typescript/src/App.tsx
@@ -1,13 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 import Button from './components/Button';
 
-function App() {
+function App()
+{
+    const [text,setText]=useState<string>("")
   return (
     <div className="App">
-          <Button color='red' text='RED' />
-      <Button color='blue' text='BLUE'/>
-      <Button color='green' text='GREEN'/>
-          
+          <Button color='red' text='RED' handleClick={()=>setText("Button color is RED")}/>
+          <Button color='blue' text='BLUE' handleClick={() => setText("Button color is BLUE")} />
+          <Button color='green' text='GREEN' handleClick={() => setText("Button color is GREEN")} />
+          {text}
     </div>
   );
 }
--- react_with_typescript/src/components/Button.tsx
+++ react_with_typescript/src/components/Button.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 type ButtonProps={
     color: "red" | "blue" | "green",
-    text:string
+    text: string,
+    handleClick: ()=> void
 }
 const Button = (props:ButtonProps) => {
   return (
       <div>
-          <button style={{backgroundColor:props.color}}>{props.text}</button>
+          <button style={{backgroundColor:props.color}} onClick={props.handleClick}>{props.text}</button>
     </div>
   )
 }
