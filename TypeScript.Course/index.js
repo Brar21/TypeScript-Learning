3d5440be4430d199f610744bc156114d4091813a
Brar21 <103635395+Brar21@users.noreply.github.com>
2022-12-25 22:34:26 +0530

    wokring on it

--- TypeScript.Course/index.js
+++ TypeScript.Course/index.js
@@ -1,3 +1,24 @@
-console.log("hello world");
-var Names = "Typescript-Learning";
-console.log(Names);
+//console.log("hello world");
+//var Names: string = "Typescript-Learning";
+//console.log(Names)
+//ENUM
+//An enum is a special "class" that represents a group of constants (unchangeable variables).
+//Enums come in two flavors string and numeric. Lets start with numeric.
+//Example of Numeric Enum :
+//enum intialization
+//{
+//    start=2,
+//    center,
+//    end
+//}
+//var myENum:intialization=intialization.center
+//console.log(myENum)
+//by default it will take value start from 0...
+//Example od string Enum:
+var StringEnum;
+(function (StringEnum) {
+    StringEnum["starting"] = "starting";
+    StringEnum["ending"] = "ending";
+})(StringEnum || (StringEnum = {}));
+var myStringEnum = StringEnum.starting;
+console.log(myStringEnum);
--- TypeScript.Course/index.ts
+++ TypeScript.Course/index.ts
@@ -1,4 +1,34 @@
-console.log("hello world");
+//console.log("hello world");
 
-var Names: string = "Typescript-Learning";
-console.log(Names)
+//var Names: string = "Typescript-Learning";
+//console.log(Names)
+
+//ENUM
+
+//An enum is a special "class" that represents a group of constants (unchangeable variables).
+
+//Enums come in two flavors string and numeric. Lets start with numeric.
+
+//Example of Numeric Enum :
+
+//enum intialization
+//{
+//    start=2,
+//    center,
+//    end
+//}
+//var myENum:intialization=intialization.center
+//console.log(myENum)
+
+//by default it will take value start from 0...
+
+//Example od string Enum:
+
+enum StringEnum
+{
+    starting = "starting",
+    ending="ending"
+}
+
+var myStringEnum: StringEnum = StringEnum.starting
+console.log(myStringEnum)
