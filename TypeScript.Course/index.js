9acae95fac4aa3c08248c1965395ee473ed434c7
Brar21 <103635395+Brar21@users.noreply.github.com>
2022-12-25 22:16:38 +0530

    working on it

---
+++ TypeScript.Course/index.js
@@ -0,0 +1,3 @@
+console.log("hello world");
+var Names = "Typescript-Learning";
+console.log(Names);
---
+++ TypeScript.Course/index.ts
@@ -0,0 +1,4 @@
+console.log("hello world");
+
+var Names: string = "Typescript-Learning";
+console.log(Names)
