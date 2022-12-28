6a06cc411f8293c074904a29651b603b328f8e67
Brar21 <103635395+Brar21@users.noreply.github.com>
2022-12-28 15:47:19 +0530

    course addon

--- Typescript-addtoCart/src/App.tsx
+++ Typescript-addtoCart/src/App.tsx
@@ -1,8 +1,14 @@
-
+import { Route,Routes } from "react-router-dom"
 function App() {
  
 
-  return <h1>hi</h1>
+    return <>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/store" element={<Store/>}/>
+            <Route path="/about" element={<About/>}/>
+    </Routes>
+    </>
     
   
 }
