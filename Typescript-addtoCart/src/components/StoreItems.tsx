eeeaa1afaf0aed94ddcb211bbca6166bd57aedb3
Brar21 <103635395+Brar21@users.noreply.github.com>
2022-12-28 16:46:15 +0530

    course addon

--- Typescript-addtoCart/src/components/StoreItems.tsx
+++ Typescript-addtoCart/src/components/StoreItems.tsx
@@ -7,9 +7,11 @@ type StoreProps = {
 const StoreItems = ({ id,name,price,imgUrl }: StoreProps) =>
 {
     return <>
-        <img src={imgUrl} alt="1" />
+        <img src={imgUrl} alt="1" style={{ width: '100%' }} />
+        <div style={{display:'flex',justifyContent:'space-between'}}>
         <h4>{name}</h4>
         <p>{price}</p>
+        </div>
         <button>Add to Cart</button>
     </>
 }
