a4a30a1ac21a126d523faf00747823749d5ed17c
Brar21 <103635395+Brar21@users.noreply.github.com>
2022-12-29 22:48:10 +0530

    working on Udemy course

--- TypeScript.Course/src/index.ts
+++ TypeScript.Course/src/index.ts
@@ -1,13 +1,3 @@
-//Tsconfig.json file
-//console.log("hello world")
-//error Handling
-//var hello:number = 2
-//console.log(hello)
-function printData(data:string):void
-{
-        console.log(data)
-    }
-    printData("hi")
 
-const HelloData = 2;
+
     
