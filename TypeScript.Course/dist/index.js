025c748e42b40c5ed6c6d4d2cdda7d25ad9ee1fc
Brar21 <103635395+Brar21@users.noreply.github.com>
2022-12-31 22:28:12 +0530

    working on Udemy course

---
+++ TypeScript.Course/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Create tsconfig.json file");
+function NName(dataName) {
+    console.log(dataName);
+}
+print();
+//# sourceMappingURL=index.js.map
--- TypeScript.Course/index.js
+++
@@ -1,1 +0,0 @@
-console.log("Create tsconfig.json file");
